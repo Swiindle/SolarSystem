fe1f6b1542667d10231539d6174bfc121d643e92
Mark <18markl@Mark-mac.local>
2019-10-19 22:50:08 +0100

    program can now read text files

--- SolarSystemManager.java
+++ SolarSystemManager.java
@@ -34,51 +34,136 @@ public class SolarSystemManager
     /**
      *
      */
-    private String[] getDataString(int i)
+    private String[] getDataString(int wantedLine)
     {
-        String[] data = new String[3];
-         
-         
-         
-        return data;
+        String[] array = new String[3];
+        try
+        {
+            File solarObjecData = new File(dataFileName);               // the file object
+            Scanner fileReader = new Scanner(solarObjecData);           // the file reader object
+            int lineCount = 0;
+            while(fileReader.hasNextLine())
+            {
+                String stringData = fileReader.nextLine();
+                //System.out.println("Line: " + stringData);
+                StringTokenizer st = new StringTokenizer(stringData,",");
+                if(lineCount == wantedLine)
+                {
+                    for(int i = 0 ; st.hasMoreTokens() ; i++)
+                    {
+                        String token = st.nextToken();
+                        //System.out.println("Token :'" + token + "', #Token in line: " + i + ", #Tokens in total: " + st.countTokens());
+                        if(i == 0)
+                        {
+                            array[0] = token;
+                        }
+                        if(i == 1)
+                        {
+                            array[1] = token;
+                        }
+                        if(i == 2)
+                        {
+                            array[2] = token;
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    lineCount++;
+                }
+            }
+            fileReader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("you may not have a SolarObjectData.txt");
+        }
+        System.out.println("Class name: " + array[0] + ", Object name: " + array[1] + ", Object color: " + array[2]);
+        return array;
     }
     
     /**
      *
      */
-     private double[] getDataNumerical(int i)
-     {
-         double[] data = {1,1,1};
-         
-         
-         return data;
-     }
+    private double[] getDataNumerical(int wantedLine)
+    {
+        double[] array = {0,0,0,0};
+        try
+        {
+            File solarObjecData = new File(dataFileName);               // the file object
+            Scanner fileReader = new Scanner(solarObjecData);           // the file reader object
+
+            int lineCount = 0;
+            
+            while(fileReader.hasNextLine())
+            {
+                String stringData = fileReader.nextLine();
+                //System.out.println("Line: " + stringData);
+                StringTokenizer st = new StringTokenizer(stringData,",");
+                if(lineCount == wantedLine)
+                {
+                    for(int i = 0 ; st.hasMoreTokens() ; i++)
+                    {
+                        String token = st.nextToken();
+                        //System.out.println("Token :'" + token + "', #Token in line: " + i + ", #Tokens in total: " + st.countTokens());
+                        if(i == 3)
+                        {
+                            array[0] = Double.valueOf(token);
+                        }
+                        if(i == 4)
+                        {
+                            array[1] = Double.valueOf(token);
+                        }
+                        if(i == 5)
+                        {
+                            array[2] = Double.valueOf(token);
+                        }
+                        if(i == 6)
+                        {
+                            array[3] = Double.valueOf(token);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    lineCount++;
+                }
+            }
+            fileReader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("you may not have a SolarObjectData.txt");
+        }
+        System.out.println("Distance: " + array[0] + ", Angle: " + array[1] + ", Diameter: " + array[2] + ", OrbitalVelocity" + array[3]);
+        return array;
+    }
     
     /**
-     *
+     * This method returns an array integer containing the number of planets, the number of moons and the total number of objects.
+     * It does this by reading a user provided text document and counting the number of lines, planets and moons.
      */
     public int[] numberOfSolarObjects()
     {
-        /*
-         * In order to detect/create the Solar Objects, the program will read from a user provided text file
-         */
-        int[] array = {0,0,0};
-        
+        int[] array = {0,0,0}; // first number is the number of planets, second number is the number of moons, third number is the total objects
         try
         {
-            File solarObjecData = new File(dataFileName);           // the file object
-            Scanner fileReader = new Scanner(solarObjecData);        // the file reader object
+            File solarObjecData = new File(dataFileName);               // the file object
+            Scanner fileReader = new Scanner(solarObjecData);           // the file reader object
             
-            while (fileReader.hasNextLine())
+            while (fileReader.hasNextLine()) // going through
             {
                 String stringData = fileReader.nextLine();
-                //stringData.useDelimiter("//");
-                System.out.println("Line: " + stringData);
+                //System.out.println("Line: " + stringData);
                 StringTokenizer st = new StringTokenizer(stringData,",");
                 for(int i = 0 ; st.hasMoreTokens() ; i++)
                 {
                     String token = st.nextToken();
-                    System.out.println("Token :'" + token + "', #Token in line: " + i + ", #Tokens in total: " + st.countTokens());
+                    //System.out.println("Token :'" + token + "', #Token in line: " + i + ", #Tokens in total: " + st.countTokens());
                     if(token.equals("Planet"))
                     {
                         array[0]++;
@@ -90,13 +175,14 @@ public class SolarSystemManager
                 }
                 array[2]++;
             }
-            //System.out.println("the data file has " + totalObjects + " lines.");
             fileReader.close();
         }
         catch (FileNotFoundException e)
         {
             System.out.println("you may not have a SolarObjectData.txt");
         }
+        //System.out.println("the data file has " + array[2]+ " lines.");
+        //System.out.println("#Planets: " + array[0] + ", #Moons: " + array[1] + ", #SolarObjects: " + array[2]);
         return array;
     }
     
@@ -105,14 +191,16 @@ public class SolarSystemManager
      */
     public void go()
     {
+        // DATA SECTION//
         /*
          * Here are the variables that this method will need
          */
         int[] solarObjectArray = this.numberOfSolarObjects();
         int numberPlanets = solarObjectArray[0];
         int numberMoons = solarObjectArray[1];
+        int numberPlanetsInstantiated = 0;
+        int numberMoonsInstantiated = 0;
         int totalObjects = solarObjectArray[2];
-        System.out.println("Number planets: " + numberPlanets + ", Number moons: " + numberMoons);
         
         /*
          * As the program now knows how many objects and what objects, it shall now instantiate it
@@ -121,18 +209,28 @@ public class SolarSystemManager
         Planet planet[] = new Planet[numberPlanets];    // the number of planets is fixed but depends on the the file
         Moon moon[] = new Moon[numberMoons];            // the number of moons is fixed but depends on the the file
         
-        for(int i = 0 ; i < numberPlanets ; i++)
+        for(int i = 0 ; i < totalObjects ; i++)
         {
+            //System.out.println("///Object#: " + i + " now getting word arguments///");
             String[] wordArguments = this.getDataString(i);
+            //System.out.println("///Object#: " + i + " now getting numerical arguments///");
             double[] numberArguments = this.getDataNumerical(i);
-            //planet[i] = new Planet(wordArguments[0],numberArguments[1],);
+            if(wordArguments[0].equals("Planet"))
+            {
+                planet[numberPlanetsInstantiated] = new Planet(wordArguments[1],numberArguments[0],numberArguments[1],numberArguments[2],wordArguments[2],numberArguments[3]);
+                numberPlanetsInstantiated++;
+            }
+            if(wordArguments[0].equals("Moon"))
+            {
+                System.out.println(i);
+                moon[numberMoonsInstantiated] = new Moon(wordArguments[1],numberArguments[0],numberArguments[1],numberArguments[2],wordArguments[2],numberArguments[3]);
+                numberMoonsInstantiated++;
+            }
         }
-        planet[0] = new Planet("Earth",50,0,10,"BLUE",1);   // can i theoratical return an object, let the object point to the reference
-        moon[0] = new Moon("Moon",20,0,5,"WHITE",1);    // passed by method, e.g file reading method returns object
     
         /*
          * Adding planets to sun's orbit, and adding moons to planet's orbit
-         */
+
         
         for(int i = 0 ; i < numberPlanets ; i++)
         {
@@ -141,8 +239,8 @@ public class SolarSystemManager
             {
                 planet[i].addMoon(moon[j]);
             }
-        }
-        
+        }         */
+        // ACTION SECTION //
         /*
          * The system has now finished its data processing and will now show the solar system in progress!
          */
